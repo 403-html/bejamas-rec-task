bd91a5f04cf619872d60c3f5ddde723f304bc20d
Tymoteusz Stępień <tymoteusz.stepien@wp.pl>
2025-04-24 09:35:37 +0200

    misc: format with prettier

--- utils/extract-links.ts
+++ utils/extract-links.ts
@@ -1,23 +1,26 @@
 import { Page } from "@playwright/test";
 
-export const extractLinks = async (page: Page, baseURL: string): Promise<string[]> => {
+export const extractLinks = async (
+  page: Page,
+  baseURL: string,
+): Promise<string[]> => {
   const links = await page.locator("a").all();
-    const allLinkHrefs = await Promise.all(
-      links.map((link) => link.getAttribute("href")),
-    );
-    
-    const validHrefs = allLinkHrefs.reduce(
-      (acc: string[], href: string | null) => {
-        if (href && !href.startsWith("#") && !href.startsWith("mailto:")) {
-          if (href.startsWith("/")) {
-            acc.push(`${baseURL}${href}`);
-          } else {
-            acc.push(href);
-          }
+  const allLinkHrefs = await Promise.all(
+    links.map((link) => link.getAttribute("href")),
+  );
+
+  const validHrefs = allLinkHrefs.reduce(
+    (acc: string[], href: string | null) => {
+      if (href && !href.startsWith("#") && !href.startsWith("mailto:")) {
+        if (href.startsWith("/")) {
+          acc.push(`${baseURL}${href}`);
+        } else {
+          acc.push(href);
         }
-        return acc;
-      },
-      [],
-    );
-    return [...new Set(validHrefs)];
-}
+      }
+      return acc;
+    },
+    [],
+  );
+  return [...new Set(validHrefs)];
+};
